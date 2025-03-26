cdd6ab8eca514d6b6f3d2564ae352e5a9afe8fe1
Boris <adrboris@outlook.com>
2025-03-26 13:09:12 +0300

    test 1

--- app/dashboard/products/[productId]/page.tsx
+++ app/dashboard/products/[productId]/page.tsx
@@ -1,8 +1,19 @@
-const ProductIdPage = async () => {
-    'use cache'
+import { Suspense } from "react"
+
+const ProductIdPage = async ({ params }: {params:Promise<{productId:string}>}) => {
+ 
     return (
-        <div>ProductIdPage</div>
+        <div>
+            <Suspense>
+               <SuspendedProduct params={params} /> 
+            </Suspense>
+        </div>
     )
 }
 
 export default ProductIdPage
+
+async function SuspendedProduct({ params }: {params:Promise<{productId:string}>}){
+    const {productId} = await params
+    return <p>{productId}</p>
+}
