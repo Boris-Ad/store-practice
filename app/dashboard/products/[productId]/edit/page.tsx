8a022bf70271f31cf1fca2b02648affe20a0567e
Boris <adrboris@outlook.com>
2025-03-26 12:29:50 +0300

    check error

--- app/dashboard/products/[productId]/edit/page.tsx
+++ app/dashboard/products/[productId]/edit/page.tsx
@@ -1,10 +1,10 @@
 import { Suspense } from 'react';
 import { AdminPagesTitle } from '@/app/dashboard/_components/AdminPagesTitle';
-import { getCategories, getCategoryById } from '@/drizzle/db/categories.db';
-import { getProductById } from '@/drizzle/db/products.db';
+import { getCategories } from '@/drizzle/db/categories.db';
+// import { getProductById } from '@/drizzle/db/products.db';
 import { Categories } from '../../_components/Categories';
 import { CategoriesSkeleton } from '@/app/dashboard/_skeletons/CategoriesSkeleton';
-import { EditProduct } from './_components/EditProduct';
+// import { EditProduct } from './_components/EditProduct';
 
 const EditProductPage = async ({ params }: { params: Promise<{ productId: string }> }) => {
   return (
@@ -16,7 +16,7 @@ const EditProductPage = async ({ params }: { params: Promise<{ productId: string
           <SuspendedCategoriesComponent />
         </Suspense>
         <Suspense>
-          <SuspendedEditProductComponent params={params} />
+          {/* <SuspendedEditProductComponent params={params} /> */}
         </Suspense>
       </section>
     </>
@@ -30,11 +30,11 @@ async function SuspendedCategoriesComponent() {
   return <Categories categories={categories} />;
 }
 
-async function SuspendedEditProductComponent({ params }: { params: Promise<{ productId: string }> }) {
-  const { productId } = await params;
-  const product = await getProductById(productId, { images: true });
-  if (product == null) return null;
-  const category = await getCategoryById(product.categoryId, {});
-  if (category == null) return null;
-  return <EditProduct product={product} category={category} />;
-}
+// async function SuspendedEditProductComponent({ params }: { params: Promise<{ productId: string }> }) {
+//   const { productId } = await params;
+//   const product = await getProductById(productId, { images: true });
+//   if (product == null) return null;
+//   const category = await getCategoryById(product.categoryId, {});
+//   if (category == null) return null;
+//   return <EditProduct product={product} category={category} />;
+// }
