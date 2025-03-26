a8fb2341ebce2090d81c9026a5723fdb103c3a60
Boris <adrboris@outlook.com>
2025-03-26 14:06:40 +0300

    not Io3

--- app/dashboard/products/[productId]/page.tsx
+++ app/dashboard/products/[productId]/page.tsx
@@ -1,12 +1,4 @@
- "use cache"
-
-import { unstable_cacheTag } from 'next/cache';
-
 const ProductIdPage = async ({ params }: { params: Promise<{ productId: string }> }) => {
-   
-
-    unstable_cacheTag('productId')
-    
   const { productId } = await params;
   return (
     <div>
